5f334603d879af1946e9bfdcd310cc3fb7377860
liqiang <liqiang@g.im>
2020-07-27 20:13:54 +0800

    增加了行业板块相关的api

--- foxrelax/__init__.py
+++ foxrelax/__init__.py
@@ -8,8 +8,11 @@ from foxrelax.common.util import (root_path, version, install_requires, md5,
 from foxrelax.client import api_client
 from foxrelax.config import auth
 
-from foxrelax.api import (echo, trade_calendar, exchange_info, stock_info,
-                          adj_factor, normalize_symbol, stock_daily,
-                          stock_weekly, stock_monthly, stock_bar,
-                          balance_sheet, income, cash_flow, stock_suspend,
-                          deposit_rate, loan_rate, cpi, cpi_area)
+from foxrelax.api import (
+    echo, trade_calendar, exchange_info, stock_info, adj_factor,
+    normalize_symbol, stock_daily, stock_weekly, stock_monthly, stock_bar,
+    balance_sheet, income, cash_flow, industry_industry, industry_concept,
+    industry_region, industry, industry_stock_industry, industry_stock_concept,
+    industry_stock_region, industry_stock, stock_industry_industry,
+    stock_industry_concept, stock_industry_region, stock_industry,
+    stock_suspend, deposit_rate, loan_rate, cpi, cpi_area)
--- foxrelax/api.py
+++ foxrelax/api.py
@@ -215,6 +215,139 @@ def cash_flow(symbol=None,
                         fields=fields)
 
 
+def industry_industry(fields=None):
+    """
+    行业板块
+    """
+
+    client = api_client()
+
+    return client.query('industry_industry', fields=fields)
+
+
+def industry_concept(fields=None):
+    """
+    概念板块
+    """
+
+    client = api_client()
+
+    return client.query('industry_concept', fields=fields)
+
+
+def industry_region(fields=None):
+    """
+    地域板块
+    """
+
+    client = api_client()
+
+    return client.query('industry_region', fields=fields)
+
+
+def industry(industry_type=None, fields=None):
+    """
+    板块(通用)
+    """
+
+    client = api_client()
+
+    return client.query('industry', industry_type=industry_type, fields=fields)
+
+
+def industry_stock_industry(industry_symbol=None, fields=None):
+    """
+    行业板块成分股
+    """
+
+    client = api_client()
+
+    return client.query('industry_stock_industry',
+                        industry_symbol=industry_symbol,
+                        fields=fields)
+
+
+def industry_stock_concept(industry_symbol=None, fields=None):
+    """
+    概念板块成分股
+    """
+
+    client = api_client()
+
+    return client.query('industry_stock_concept',
+                        industry_symbol=industry_symbol,
+                        fields=fields)
+
+
+def industry_stock_region(industry_symbol=None, fields=None):
+    """
+    地域板块成分股
+    """
+
+    client = api_client()
+
+    return client.query('industry_stock_region',
+                        industry_symbol=industry_symbol,
+                        fields=fields)
+
+
+def industry_stock(industry_symbol=None, fields=None):
+    """
+    板块成分股(通用)
+    """
+
+    client = api_client()
+
+    return client.query('industry_stock',
+                        industry_symbol=industry_symbol,
+                        fields=fields)
+
+
+def stock_industry_industry(symbol=None, fields=None):
+    """
+    股票所属行业板块
+    """
+
+    client = api_client()
+
+    return client.query('stock_industry_industry',
+                        symbol=symbol,
+                        fields=fields)
+
+
+def stock_industry_concept(symbol=None, fields=None):
+    """
+    股票所属概念板块
+    """
+
+    client = api_client()
+
+    return client.query('stock_industry_concept', symbol=symbol, fields=fields)
+
+
+def stock_industry_region(symbol=None, fields=None):
+    """
+    股票所属地域板块
+    """
+
+    client = api_client()
+
+    return client.query('stock_industry_region', symbol=symbol, fields=fields)
+
+
+def stock_industry(symbol=None, industry_type=None, fields=None):
+    """
+    股票所属板块(通用)
+    """
+
+    client = api_client()
+
+    return client.query('stock_industry',
+                        symbol=symbol,
+                        industry_type=industry_type,
+                        fields=fields)
+
+
 def stock_suspend(symbol=None,
                   suspend_date=None,
                   resume_date=None,
