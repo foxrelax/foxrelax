9bf611738b839013fabbe191303f2db8e9477f50
liqiang <liqiang@g.im>
2020-08-15 10:26:57 +0800

    更新了setup

--- setup.py
+++ setup.py
@@ -1,23 +1,52 @@
 # -*- coding:utf-8 -*-
+import os
+import io
 from setuptools import (setup, find_packages)
-import foxrelax as relax
 
 long_desc = """
 FoxRelax
 """
 
+
+def root_path():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def version():
+    fpath = os.path.join(root_path(), 'foxrelax', 'version.txt')
+    with open(fpath, 'r') as f:
+        f = open(fpath, 'r')
+        line = f.readline()
+        ver = line.strip()
+
+    return ver
+
+
+def install_requires():
+    fpath = os.path.join(root_path(), 'requirements.txt')
+    requires = []
+
+    with open(fpath, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line and line[0] != '#':
+                requires.append(line)
+
+    return requires
+
+
 setup(
     name='foxrelax',
-    version=relax.version(),
+    version=version(),
     description='foxrelax',
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description=io.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='liqiang',
     author_email='liqiang@g.im',
     license='BSD License',
     platforms=['all'],
     url='https://github.com/foxrelax/foxrelax',
-    install_requires=relax.install_requires(),
+    install_requires=install_requires(),
     keywords='foxrelax',
     packages=find_packages(),
     include_package_data=True,
