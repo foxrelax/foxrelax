d12a63c7b493f9daa6f76d5b8e0a1ac4f016d7ce
liqiang <liqiang@g.im>
2021-04-04 08:03:25 +0800

    补充缺失的export APIs

--- foxrelax/__init__.py
+++ foxrelax/__init__.py
@@ -9,8 +9,11 @@ from foxrelax.client import api_client
 from foxrelax.config import auth
 
 from foxrelax.api import (
-    echo, trade_calendar, area, exchange_info, stock_info, adj_factor,
-    normalize_stock_symbol, stock_daily, stock_weekly, stock_monthly, stock_bar,
+    echo, trade_calendar, area, exchange_info, stock_info, adj_factor, bar,
+    index_info, index_daily, index_weekly, index_monthly, index_bar,
+    money_flow_hk, money_flow_hk_stat, rise_fall_stat, limit_rise_fall_stat,
+    normalize_symbol, normalize_stock_symbol, normalize_index_symbol,
+    news_flash, stock_daily, stock_weekly, stock_monthly, stock_bar,
     balance_sheet, income, cash_flow, industry_industry, industry_concept,
     industry_region, industry, industry_stock_industry, industry_stock_concept,
     industry_stock_region, industry_stock, stock_industry_industry,
