86c8279a558a51879bdc34faace3fc1b0ea08d83
liqiang <liqiang@g.im>
2021-08-31 12:47:02 +0800

    update

--- foxrelax/ml/torch.py
+++ foxrelax/ml/torch.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 import time
 import os
+import re
+import collections
+import random
 import hashlib
 import requests
 import numpy as np
@@ -26,6 +29,8 @@ DATA_HUB['kaggle_house_train'] = ('csv', '/ml/kaggle_house_pred_train.csv',
                                   '585e9cc93e70b39160e7921475f9bcd7d31219ce')
 DATA_HUB['kaggle_house_test'] = ('csv', '/ml/kaggle_house_pred_test.csv',
                                  'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+DATA_HUB['time_machine'] = ('txt', '/ml/time_machine.txt',
+                            '090b5e7e70c295757f55df93cb0a180b9691891a')
 
 
 def download(name, cache_dir=os.path.join('..', 'data')):
@@ -415,3 +420,129 @@ def train_gpu(net, train_iter, test_iter, loss, num_epochs, lr, device=None):
           f'test acc {test_acc:.3f}')
     print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
           f'on {str(device)}')
+
+
+def tokenize(lines, token='word'):
+    """Split text lines into word or character tokens"""
+    assert token in ('word', 'char'), 'Unknown token type: ' + token
+    return [line.split() if token == 'word' else list(line) for line in lines]
+
+
+class Vocab:
+    """Vocabulary for text"""
+    def __init__(self, tokens=[], min_freq=0, reserved_tokens=[]):
+        # Flatten a 2D list if needed
+        if tokens and isinstance(tokens[0], list):
+            tokens = [token for line in tokens for token in line]
+        # Count token frequencies
+        counter = collections.Counter(tokens)
+        self.token_freqs = sorted(counter.items(),
+                                  key=lambda x: x[1],
+                                  reverse=True)
+        # The list of unique tokens
+        self.idx_to_token = list(
+            sorted(
+                set(['<unk>'] + reserved_tokens + [
+                    token
+                    for token, freq in self.token_freqs if freq >= min_freq
+                ])))
+        self.token_to_idx = {
+            token: idx
+            for idx, token in enumerate(self.idx_to_token)
+        }
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):  # Index for the unknown token
+        return self.token_to_idx['<unk>']
+
+
+def load_corpus_time_machine(max_tokens=-1):
+    """返回时光机器数据集的词元索引列表和词汇表"""
+    with open(download('time_machine'), 'r') as f:
+        lines = f.readlines()
+    lines = [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+    tokens = tokenize(lines, 'word')
+    vocab = Vocab(tokens)
+    # 因为时光机器数据集中的每个文本行不一定是一个句子或一个段落,
+    # 所以将所有文本行展平到一个列表中
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
+
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    """使用随机抽样生成一个小批量子序列"""
+    # 从随机偏移量开始对序列进行分区, 随机范围包括`num_steps - 1`
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    # 减去1, 是因为我们需要考虑标签
+    num_subseqs = (len(corpus) - 1) // num_steps
+    # ⻓度为`num_steps`的子序列的起始索引
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    # 在随机抽样的迭代过程中,
+    # 来自两个相邻的、随机的、小批量中的子序列不一定在原始序列上相邻
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        # 返回从`pos`位置开始的⻓度为`num_steps`的一个序列
+        return corpus[pos:pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+
+    for i in range(0, batch_size * num_batches, batch_size):
+        # 在这里, `initial_indices`包含子序列的随机起始索引
+        initial_indices_per_batch = initial_indices[i:i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):
+    """使用顺序分区生成一个小批量子序列"""
+    # 从随机偏移量开始划分序列
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - 1 - offset) // batch_size) * batch_size
+    Xs = torch.tensor(corpus[offset:offset + num_tokens])
+    Ys = torch.tensor(corpus[offset + 1:offset + 1 + num_tokens])
+    Xs, Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_steps * num_batches, num_steps):
+        X = Xs[:, i:i + num_steps]
+        Y = Ys[:, i:i + num_steps]
+        yield X, Y
+
+
+class SeqDataLoader:
+    """加载序列数据的迭代器"""
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_steps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
+
+def load_data_time_machine(batch_size,
+                           num_steps,
+                           use_random_iter=False,
+                           max_tokens=100000):
+    data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter,
+                              max_tokens)
+    return data_iter, data_iter.vocab
