18258713e5423c7f15ef992a4dff24fe90080d16
liqiang <liqiang@g.im>
2021-09-08 15:31:18 +0800

    update

--- foxrelax/ml/torch.py
+++ foxrelax/ml/torch.py
@@ -266,6 +266,30 @@ def get_dataloader_workers(num_workers=4):
     return num_workers
 
 
+def load_data_mnist(batch_size, resize=None, root='../data'):
+    """Download the MNIST dataset and then load it into memory."""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.MNIST(root=root,
+                                             train=True,
+                                             transform=trans,
+                                             download=True)
+    mnist_test = torchvision.datasets.MNIST(root=root,
+                                            train=False,
+                                            transform=trans,
+                                            download=True)
+    return (data.DataLoader(mnist_train,
+                            batch_size,
+                            shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test,
+                            batch_size,
+                            shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
 def load_data_fashion_mnist(batch_size, resize=None, root='../data'):
     """Download the Fashion-MNIST dataset and then load it into memory."""
     trans = [transforms.ToTensor()]
@@ -980,3 +1004,138 @@ class EncoderDecoder(nn.Module):
         enc_outputs = self.encoder(enc_X, *args)
         dec_state = self.decoder.init_state(enc_outputs, *args)
         return self.decoder(dec_X, dec_state)
+
+
+def sequence_mask(X, valid_len, value=0):
+    """在序列中屏蔽不相关的项"""
+    maxlen = X.size(1)
+    mask = torch.arange((maxlen), dtype=torch.float32,
+                        device=X.device)[None, :] < valid_len[:, None]
+    X[~mask] = value
+    return X
+
+
+class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
+    """带遮蔽的softmax交叉熵损失函数"""
+    def forward(self, pred, label, valid_len):
+        # `pred` 的形状：(`batch_size`, `num_steps`, `vocab_size`)
+        # `label` 的形状：(`batch_size`, `num_steps`)
+        # `valid_len` 的形状：(`batch_size`,)
+        weights = torch.ones_like(label)
+        weights = sequence_mask(weights, valid_len)
+        self.reduction = 'none'
+        # `unweighted_loss`的形状: (`batch_size`, `num_steps`)
+        unweighted_loss = super(MaskedSoftmaxCELoss,
+                                self).forward(pred.permute(0, 2, 1), label)
+        # `weighted_loss`的形状: (`batch_size`,)
+        weighted_loss = (weights * unweighted_loss).mean(dim=1)
+        return weighted_loss
+
+
+def train_seq2seq_gpu(net, data_iter, lr, num_epochs, tgt_vocab, device=None):
+    """训练序列到序列模型"""
+    if device is None:
+        device = try_gpu()
+
+    def xavier_init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.xavier_normal_(m.weight)
+        elif type(m) == nn.GRU:
+            for param in m._flat_weights_names:
+                if 'weight' in param:
+                    nn.init.xavier_normal_(m._parameters[param])
+
+    net.apply(xavier_init_weights)
+    net.to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    loss = MaskedSoftmaxCELoss()
+    net.train()
+    animator = Animator(xlabel='epoch', ylabel='loss', xlim=[10, num_epochs])
+    for epoch in range(num_epochs):
+        timer = Timer()
+        metric = Accumulator(2)  # 训练损失总和, 词元数量
+        for batch in data_iter:
+            optimizer.zero_grad()
+            X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
+            bos = torch.tensor([tgt_vocab['<bos>']] * Y.shape[0],
+                               device=device).reshape(-1, 1)
+            dec_input = torch.cat((bos, Y[:, :-1]), 1)
+            Y_hat, _ = net(X, dec_input, X_valid_len)
+            l = loss(Y_hat, Y, Y_valid_len)
+            l.sum().backward()  # 损失函数的标量进行'反传'
+            grad_clipping(net, 1)
+            num_tokens = Y_valid_len.sum()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l.sum(), num_tokens)
+            if (epoch + 1) % 10 == 0:
+                animator.add(epoch + 1, (metric[0] / metric[1], ))
+    print(f'loss {metric[0] / metric[1]:.3f}, {metric[1] / timer.stop():.1f} '
+          f'tokens/sec on {str(device)}')
+
+
+def predict_seq2seq(net,
+                    src_setence,
+                    src_vocab,
+                    tgt_vocab,
+                    num_steps,
+                    device,
+                    save_attention_weights=False):
+    """序列到序列模型的预测"""
+    # 在预测时将`net`设置为评估模式
+    net.eval()
+    src_tokens = src_vocab[src_setence.lower().split(' ')] + [
+        src_vocab['<eos>']
+    ]
+    enc_valid_len = torch.tensor([len(src_tokens)], device=device)
+    src_tokens = truncate_pad(src_tokens, num_steps, src_vocab['<pad>'])
+    # 添加batch_size轴
+    # `enc_X`的形状是: (`1, `num_steps`)
+    enc_X = torch.unsqueeze(torch.tensor(src_tokens,
+                                         dtype=torch.long,
+                                         device=device),
+                            dim=0)
+    # `enc_outputs[0]`的形状: (`num_steps`, `1`, `num_hiddens`)
+    # `enc_outputs[1]`的形状: (`num_layers`, `1`, `num_hiddens`)
+    enc_outputs = net.encoder(enc_X, enc_valid_len)
+    dec_state = net.decoder.init_state(enc_outputs, enc_valid_len)
+    # 添加batch_size轴
+    # `dec_X`的形状是: (`1, `1`)
+    dec_X = torch.unsqueeze(torch.tensor([tgt_vocab['<bos>']],
+                                         dtype=torch.long,
+                                         device=device),
+                            dim=0)
+    output_seq, attention_weight_seq = [], []
+    for _ in range(num_steps):
+        # `Y`的形状: (`1`, `1`, `vocab_size`)
+        # `dec_state`的形状: (`num_layers`, `1`, `num_hiddens`)
+        Y, dec_state = net.decoder(dec_X, dec_state)
+        # `dec_X`的形状: (`1`, `1`)
+        # 我们使用具有预测最高可能性的token, 作为解码器在下一时间步的输入
+        dec_X = Y.argmax(dim=2)
+        pred = dec_X.squeeze(dim=0).type(torch.int32).item()
+        # 保存注意力权重
+        if save_attention_weights:
+            attention_weight_seq.append(net.decoder.attention_weights)
+        # 一旦序列结束词元被预测, 输出序列的生成就完成了
+        if pred == tgt_vocab['<eos>']:
+            break
+        output_seq.append(pred)
+    return ' '.join(tgt_vocab.to_tokens(output_seq)), attention_weight_seq
+
+
+def bleu(pred_seq, label_seq, k):
+    """计算 BLEU"""
+    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_label / len_pred))
+    for n in range(1, k + 1):
+        num_matches, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs[''.join(label_tokens[i:i + n])] += 1
+        for i in range(len_pred - n + 1):
+            if label_subs[''.join(pred_tokens[i:i + n])] > 0:
+                num_matches += 1
+                label_subs[''.join(pred_tokens[i:i + n])] -= 1
+        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
+    return score
