ce86522a773aaf3c91b3d2ad661b3fe506877dda
liqiang <liqiang@g.im>
2021-10-15 12:22:45 +0800

    update

--- foxrelax/ml/torch.py
+++ foxrelax/ml/torch.py
@@ -1448,6 +1448,149 @@ def show_bboxes(axes, bboxes, labels=None, colors=None):
                       bbox=dict(facecolor=color, lw=0))
 
 
+def box_iou(boxes1, boxes2):
+    """计算两个锚框或边界框列表中成对的交并比"""
+    box_area = lambda boxes: ((boxes[:, 2] - boxes[:, 0]) *
+                              (boxes[:, 3] - boxes[:, 1]))
+    # `boxes1`, `boxes2`, `areas1`, `areas2`的形状:
+    # `boxes1`: (boxes1的数量, 4)
+    # `boxes2`: (boxes2的数量, 4)
+    # `areas1`: (boxes1的数量,)
+    # `areas2`: (boxes2的数量,)
+    areas1 = box_area(boxes1)
+    areas2 = box_area(boxes2)
+    # `inter_upperlefts`, `inter_lowerrights`, `inters`的形状:
+    # (boxes1的数量, boxes2的数量, 2)
+    inter_upperlefts = torch.max(boxes1[:, None, :2], boxes2[:, :2])
+    inter_lowerrights = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])
+    inters = (inter_lowerrights - inter_upperlefts).clamp(min=0)
+    # `inter_areas` and `union_areas`的形状:
+    # (boxes1的数量, boxes2的数量)
+    inter_areas = inters[:, :, 0] * inters[:, :, 1]
+    union_areas = areas1[:, None] + areas2 - inter_areas
+    return inter_areas / union_areas
+
+
+def assign_anchor_to_bbox(ground_truth, anchors, device, iou_threshold=0.5):
+    """将最接近的真实边界框分配给锚框"""
+    num_anchors, num_gt_boxes = anchors.shape[0], ground_truth.shape[0]
+    # 位于第i行和第j列的元素x_ij是锚框i和真实边界框j的IoU
+    # jaccard.shape - (num_anchors, num_gt_boxes)
+    jaccard = box_iou(anchors, ground_truth)
+
+    # 对于每个锚框, 分配的真实边界框的张量
+    anchors_bbox_map = torch.full((num_anchors, ),
+                                  -1,
+                                  dtype=torch.long,
+                                  device=device)
+    # 根据阈值, 决定是否分配真实边界框
+    # max_ious.shape - (num_anchors, )
+    # indices.shape - (num_anchors, )
+    max_ious, indices = torch.max(jaccard, dim=1)
+    anc_i = torch.nonzero(max_ious >= iou_threshold).reshape(-1)
+    box_j = indices[max_ious >= iou_threshold]
+    anchors_bbox_map[anc_i] = box_j
+    col_discard = torch.full((num_anchors, ), -1)
+    row_discard = torch.full((num_gt_boxes, ), -1)
+    for _ in range(num_gt_boxes):
+        max_idx = torch.argmax(jaccard)
+        box_idx = (max_idx % num_gt_boxes).long()  # 列索引
+        anc_idx = (max_idx / num_gt_boxes).long()  # 行索引
+        anchors_bbox_map[anc_idx] = box_idx
+        jaccard[:, box_idx] = col_discard
+        jaccard[anc_idx, :] = row_discard
+
+    # anchors_bbox_map.shape - (num_anchors, )
+    return anchors_bbox_map
+
+
+def offset_boxes(anchors, assigned_bb, eps=1e-6):
+    """对锚框偏移量的转换"""
+    # c_anc.shape - (num_anchors, 4)
+    # c_assigned_bb.shape - (num_anchors, 4)
+    c_anc = box_corner_to_center(anchors)
+    c_assigned_bb = box_corner_to_center(assigned_bb)
+    # offset_xy.shape - (num_anchors, 2)
+    # offset_wh.shape - (num_anchors, 2)
+    offset_xy = 10 * (c_assigned_bb[:, :2] - c_anc[:, :2]) / c_anc[:, 2:]
+    offset_wh = 5 * torch.log(eps + c_assigned_bb[:, 2:] / c_anc[:, 2:])
+    offset = torch.cat([offset_xy, offset_wh], axis=1)
+
+    # offset.shape - (num_anchors, 4)
+    return offset
+
+
+def multibox_target(anchors, labels):
+    """使用真实边界框标记锚框"""
+    # 参数:
+    # anchors.shape - (batch_size, num_anchors, 4)
+    # labels.shape - (batch_size, num_gt_boxes, 5)
+
+    # 处理之后:
+    # anchors.shape - (num_anchors, 4)
+    batch_size, anchors = labels.shape[0], anchors.squeeze(0)
+    batch_offset, batch_mask, batch_class_labels = [], [], []
+    device, num_anchors = anchors.device, anchors.shape[0]
+    for i in range(batch_size):
+        # label.shape - (num_gt_boxes, 5)
+        label = labels[i, :, :]
+        # anchors_bbox_map.shape(num_anchors, )
+        anchors_bbox_map = assign_anchor_to_bbox(label[:, 1:], anchors, device)
+        # bbox_mask.shape(num_anchors, 4)
+        # e.g.
+        # tensor([[0., 0., 0., 0.],
+        #         [1., 1., 1., 1.],
+        #         [1., 1., 1., 1.],
+        #         [0., 0., 0., 0.],
+        #         [1., 1., 1., 1.]])
+        bbox_mask = ((anchors_bbox_map >= 0).float().unsqueeze(-1)).repeat(
+            1, 4)
+        # 将`类标签`和`分配的边界框坐标`初始化为零
+        class_labels = torch.zeros(num_anchors,
+                                   dtype=torch.long,
+                                   device=device)
+        assigned_bb = torch.zeros((num_anchors, 4),
+                                  dtype=torch.float32,
+                                  device=device)
+        # 使用真实边界框来标记锚框的类别
+        # 如果一个锚框没有被分配, 我们标记其为背景(值为零)
+        # indices_true.shape - (正类锚框的数量, 1)
+        # bb_idx.shape - (正类锚框的数量, 1)
+        indices_true = torch.nonzero(anchors_bbox_map >= 0)
+        bb_idx = anchors_bbox_map[indices_true]
+        # class_labels.shape - (num_anchors, )
+        # assigned_bb.shape - (num_anchors, 4)
+        class_labels[indices_true] = label[bb_idx, 0].long() + 1
+        assigned_bb[indices_true] = label[bb_idx, 1:]
+
+        # 偏移量转换
+        # offset.shape - (num_anchors, 4)
+        offset = offset_boxes(anchors, assigned_bb) * bbox_mask
+        batch_offset.append(offset.reshape(-1))  # (num_enchors*4, )
+        batch_mask.append(bbox_mask.reshape(-1))  # (num_echors*4, )
+        batch_class_labels.append(class_labels)  # (num_anchors, )
+
+    bbox_offset = torch.stack(batch_offset)  # (batch_size, num_enchors*4)
+    bbox_mask = torch.stack(batch_mask)  # (batch_size, num_enchors*4)
+    class_labels = torch.stack(batch_class_labels)  # (batch_size, num_enchors)
+    return (bbox_offset, bbox_mask, class_labels)
+
+def offset_inverse(anchors, offset_preds):
+    """根据带有预测偏移量的锚框来预测边界框"""
+    # anc.shape - (num_anchors, 4)
+    anc = box_corner_to_center(anchors)
+
+    # pred_bbox_xy.shape - (num_anchors, 2)
+    # pred_bbox_wh.shape - (num_anchors, 2)
+    pred_bbox_xy = (offset_preds[:, :2] * anc[:, 2:] / 10) + anc[:, :2]
+    pred_bbox_wh = torch.exp(offset_preds[:, 2:] / 5) * anc[:, 2:]
+
+    # pred_bbox.shape - (num_anchors, 4)
+    # predicted_bbox.shape - (num_anchors, 4)
+    pred_bbox = torch.cat((pred_bbox_xy, pred_bbox_wh), axis=1)
+    predicted_bbox = box_center_to_corner(pred_bbox)
+    return predicted_bbox
+
 # 目录结构(图片的尺寸都是256x256):
 # banana_detection/
 #   bananas_train/
@@ -1486,6 +1629,10 @@ def read_data_bananas(is_train=True):
                              'images', f'{img_name}')))
         # label,xmin,ymin,xmax,ymax
         targets.append(list(target))
+
+    # 图片尺寸的大小是256x256, 最终除以256表示坐标在图片的相对位置
+    # image是一个长度为1000的list, 每个元素image.shape: (3, 256, 256)
+    # targets.shape: (1000, 1, 5)
     return images, torch.tensor(targets).unsqueeze(1) / 256
 
 
