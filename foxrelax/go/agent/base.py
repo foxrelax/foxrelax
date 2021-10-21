70418672ed745a9b88aae906138ed6d36b9a13fc
liqiang <liqiang@g.im>
2021-10-21 19:33:19 +0800

    update

--- foxrelax/go/agent/base.py
+++ foxrelax/go/agent/base.py
@@ -8,6 +8,9 @@ class Agent:
         pass
 
     def select_move(self, game_state):
+        """
+        所有的机器人都要根据当前的游戏状态选择一个动作
+        """
         raise NotImplemented()
 
     def diagnostics(self):
