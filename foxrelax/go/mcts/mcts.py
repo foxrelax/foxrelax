8d64c938a2ad6153a66a626490f770a4448e6c0a
liqiang <liqiang@g.im>
2021-10-26 18:51:57 +0800

    update

--- foxrelax/go/mcts/mcts.py
+++ foxrelax/go/mcts/mcts.py
@@ -1,1 +1,25 @@
 # -*- coding:utf-8 -*-
+import math
+import random
+
+from foxrelax.go.agent.base import Agent
+from foxrelax.go.gotypes import Player
+from foxrelax.go.utils import coords_from_point
+
+__all__ = [
+    'MCTSAgent',
+]
+
+
+def fmt(x):
+    # 格式化Player
+    if x is Player.BLACK:
+        return 'B'
+    if x is Player.WHITE:
+        return 'W'
+    # 格式化Move
+    if x.is_pass:
+        return 'pass'
+    if x.is_resign:
+        return 'resign'
+    return coords_from_point(x.point)
