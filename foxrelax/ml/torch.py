5873db1a009d2a0bcab3ef279e57f332c8c7c81e
liqiang <liqiang@g.im>
2021-11-16 18:26:19 +0800

    update

--- foxrelax/ml/torch.py
+++ foxrelax/ml/torch.py
@@ -161,7 +161,21 @@ def plot(X,
          fmts=('-', 'm--', 'g-.', 'r:'),
          figsize=(6, 4),
          axes=None):
-    """Plot data points."""
+    """
+    画图(可以同时画多条曲线)
+    
+    Usage:
+    # 生成数据
+    x = np.linspace(1, 10, 100)
+    y1, y2 = np.sin(x), np.cos(x)
+
+    # 画图
+    ml.plot(y1)  # 使用0-100作为作为x轴来画图
+    ml.plot([y1, y2])  # 使用0-100作为作为x轴来画图
+    ml.plot(x, y1)
+    ml.plot(x, [y1, y2])
+    ml.plot([x, x], [y1, y2])
+    """
     if legend is None:
         legend = []
 
@@ -1761,3 +1775,32 @@ def train_concise_optimization(trainer_fn,
                 timer.start()
     print(f'loss: {animator.Y[0][-1]:.3f}, {timer.avg():.3f} sec/epoch')
     return timer.cumsum(), animator.Y[0]
+
+
+def show_heatmaps(matrices,
+                  xlabel,
+                  ylabel,
+                  titles=None,
+                  figsize=(3.5, 3.5),
+                  cmap='Reds'):
+    """
+    matrices的形状是: (要显示的行数, 要显示的列数, 查询的数目, 键的数目)
+    """
+    use_svg_display()
+    num_rows, num_cols = matrices.shape[0], matrices.shape[1]
+    fig, axes = plt.subplots(num_rows,
+                             num_cols,
+                             figsize=figsize,
+                             sharex=True,
+                             sharey=True,
+                             squeeze=False)
+    for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+        for j, (ax, matrix) in enumerate(zip(row_axes, row_matrices)):
+            pcm = ax.imshow(matrix.detach().numpy(), cmap=cmap)
+            if i == num_rows - 1:
+                ax.set_xlabel(xlabel)
+            if j == 0:
+                ax.set_ylabel(ylabel)
+            if titles:
+                ax.set_title(titles[j])
+    fig.colorbar(pcm, ax=axes, shrink=0.6)
