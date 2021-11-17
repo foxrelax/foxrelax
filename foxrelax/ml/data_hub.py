7751d9a26a29a9fa6b3e0e1f05420cd221f98ad3
liqiang <liqiang@g.im>
2021-11-17 19:25:16 +0800

    update

---
+++ foxrelax/ml/data_hub.py
@@ -0,0 +1,126 @@
+# -*- coding:utf-8 -*-
+
+DATA_URL = 'http://oss.foxrelax.com'
+DATA_HUB = dict()
+DATA_HUB['logistic_regression_unseparable'] = (
+    'csv', '/ml/logistic_regression_unseparable.csv',
+    'f7a9c08c4b11fa8e03367a996ac75e5539e25c36',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/logistic_regression_unseparable.csv'
+)
+DATA_HUB['logistic_regression_separable'] = (
+    'csv', '/ml/logistic_regression_separable.csv',
+    '97ff298c6aed70329f6f28757de04759f5600956',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/logistic_regression_separable.csv'
+)
+DATA_HUB['airfoil_self_noise'] = (
+    'dat', '/ml/airfoil_self_noise.dat',
+    '76e5be1548fd8222e5074cf0faae75edff8cf93f',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/airfoil_self_noise.dat')
+DATA_HUB['mnist'] = (
+    'npz', '/ml/mnist.npz', 'e7727b719822ece6d78ca83af078bcd85ae81b0d',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/mnist.npz')
+DATA_HUB['kaggle_house_train'] = (
+    'csv', '/ml/kaggle_house_pred_train.csv',
+    '585e9cc93e70b39160e7921475f9bcd7d31219ce',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/kaggle_house_pred_train.csv'
+)
+DATA_HUB['kaggle_house_test'] = (
+    'csv', '/ml/kaggle_house_pred_test.csv',
+    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/kaggle_house_pred_test.csv'
+)
+DATA_HUB['time_machine'] = (
+    'txt', '/ml/time_machine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/time_machine.txt')
+DATA_HUB['fra_eng'] = (
+    'zip', '/ml/fra_eng.zip', '02eee9efbc64e076be914c6c163740dd5d448b36',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/fra_eng.zip')
+DATA_HUB['hotdog'] = (
+    'zip', '/ml/hotdog.zip', 'fba480ffa8aa7e0febbb511d181409f899b9baa5',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/hotdog.zip')
+DATA_HUB['banana_detection'] = (
+    'zip', '/ml/banana_detection.zip',
+    '068e13f04d30c8b96d30e490394ce9ab7cbdf2d5',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/banana_detection.zip')
+DATA_HUB['img/cat1'] = (
+    'jpg', '/ml/img/cat1.jpg', 'f9c5b905d908b97eeeb64ff34a46fa8b143f88f8',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/img/cat1.jpg')
+DATA_HUB['img/cat2'] = (
+    'jpg', '/ml/img/cat2.jpg', 'b712adcb9ca6af53081bd96426b719573f40053e',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/img/cat2.jpg')
+DATA_HUB['img/cat3'] = (
+    'jpg', '/ml/img/cat3.jpg', '80249a6aa841706d861f3f7efad582f6828ca3d0',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/img/cat3.jpg')
+DATA_HUB['img/catdog'] = (
+    'jpg', '/ml/img/catdog.jpg', '60b7d540db03eef6b9834329bccc4417ef349bf6',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/img/catdog.jpg')
+DATA_HUB['go/features-20'] = (
+    'npy', '/ml/go/features-20.npy',
+    '3f84459adb1d5dd66a6395dfbc513b99349fd8f0',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/go/features-20.npy')
+DATA_HUB['go/features-200'] = (
+    'npy', '/ml/go/features-200.npy',
+    'cb8628ffc84c9b6ecb1bd0991255b7bb3d85b0d7',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/go/features-200.npy')
+DATA_HUB['go/features-40k'] = (
+    'npy', '/ml/go/features-40k.npy',
+    'f3f0bdb3dd8a5cc663ef56ca4c8e06032034531d',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/go/features-40k.npy')
+DATA_HUB['go/labels-20'] = (
+    'npy', '/ml/go/labels-20.npy', 'c6f03c99916218175e185601530fc6a32d26c114',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/go/labels-20.npy')
+DATA_HUB['go/labels-200'] = (
+    'npy', '/ml/go/labels-200.npy', 'ef7789e4b68555c903bc66ec56ecbf7af956d081',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/go/labels-200.npy')
+DATA_HUB['go/labels-40k'] = (
+    'npy', '/ml/go/labels-40k.npy', 'd959a562ef5189413a0d1b3525972e9c9dd2b598',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/go/labels-40k.npy')
+
+DATA_HUB['dlwpt/audio-chirp'] = (
+    'zip', '/ml/dlwpt/audio-chirp.zip',
+    '9f76a2703b2efc92d942a92e46acea76e6059db8',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/audio-chirp.zip')
+DATA_HUB['dlwpt/bike-sharing-dataset'] = (
+    'zip', '/ml/dlwpt/bike-sharing-dataset.zip',
+    'fc8e284c60658bb4121782a424cbc4e955ebd3d6',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/bike-sharing-dataset.zip'
+)
+DATA_HUB['dlwpt/image-cats'] = (
+    'zip', '/ml/dlwpt/image-cats.zip',
+    '45f71d8fca66d81f9a0218ed0df87caf8beb5962',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/image-cats.zip')
+DATA_HUB['dlwpt/image-dog'] = (
+    'zip', '/ml/dlwpt/image-dog.zip',
+    '13d7bfdf4a1ad781977fb3ffe85a21a8dc18ea84',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/image-dog.zip')
+
+DATA_HUB['dlwpt/jane-austen'] = (
+    'zip', '/ml/dlwpt/jane-austen.zip',
+    'da49eb304b63fff1f3a385e75f8a2076a9b823a6',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/jane-austen.zip')
+DATA_HUB['dlwpt/tabular-wine'] = (
+    'zip', '/ml/dlwpt/tabular-wine.zip',
+    'e5c949b1bf4c47bd41a85c950d96a3a16a0d608b',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/tabular-wine.zip')
+DATA_HUB['dlwpt/video-cockatoo'] = (
+    'zip', '/ml/dlwpt/video-cockatoo.zip',
+    '71213219987ecd54e40ce5154cf28f7ab8d4ea91',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/video-cockatoo.zip'
+)
+DATA_HUB['dlwpt/volumetric-dicom'] = (
+    'zip', '/ml/dlwpt/volumetric-dicom.zip',
+    'd6689fd995a22328db45d969a22a364c27dc9a29',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/volumetric-dicom.zip'
+)
+
+DATA_HUB['dlwpt/birds_vs_airplanes'] = (
+    'pt', '/ml/dlwpt/birds_vs_airplanes.pt',
+    'fd866137db894f6026361745a92d845b911bc8ba',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/birds_vs_airplanes.pt'
+)
+
+DATA_HUB['dlwpt/cls_val_example'] = (
+    'pt', '/ml/dlwpt/cls_val_example.pt',
+    '12d72732b6b7d3aa8fab98bf9207d166c2d22b16',
+    'https://foxrelax.oss-cn-hangzhou.aliyuncs.com/ml/dlwpt/cls_val_example.pt'
+)
