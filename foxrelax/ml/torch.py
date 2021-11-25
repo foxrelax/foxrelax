26d3608bbf7bbd9dbc92806b93624a8811303bf9
liqiang <liqiang@g.im>
2021-11-25 11:53:39 +0800

    update

--- foxrelax/ml/torch.py
+++ foxrelax/ml/torch.py
@@ -569,8 +569,26 @@ class Vocab:
         return self.token_to_idx['<unk>']
 
 
-def load_corpus_time_machine(max_tokens=-1, token='word'):
-    """返回时光机器数据集的词元索引列表和词汇表"""
+def load_corpus_time_machine(max_tokens=-1, token='char'):
+    """
+    返回时光机器数据集的`词元索引列表`和`词汇表`
+    
+    >>> corpus, vocab = load_corpus_time_machine(token='char')
+    >>> print(f'corpus.len={len(corpus)}, vocab.len={len(vocab)}')
+    >>> print(corpus[:10])
+    >>> print(vocab.to_tokens(corpus[:10]))
+    corpus.len=170580, vocab.len=28
+    [21, 9, 6, 0, 21, 10, 14, 6, 0, 14]
+    ['t', 'h', 'e', ' ', 't', 'i', 'm', 'e', ' ', 'm']
+
+    >>> corpus, vocab = load_corpus_time_machine(token='word')
+    >>> print(f'corpus.len={len(corpus)}, vocab.len={len(vocab)}')
+    >>> print(corpus[:10])
+    >>> print(vocab.to_tokens(corpus[:10]))
+    corpus.len=32775, vocab.len=4580
+    [4041, 4108, 2414, 504, 1807, 1657, 4444, 1992, 4041, 4108]
+    ['the', 'time', 'machine', 'by', 'h', 'g', 'wells', 'i', 'the', 'time']
+    """
     with open(download('time_machine'), 'r') as f:
         lines = f.readlines()
     lines = [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
@@ -644,7 +662,17 @@ def load_data_time_machine(batch_size,
                            num_steps,
                            use_random_iter=False,
                            max_tokens=100000,
-                           token='word'):
+                           token='char'):
+    """
+    返回的data_iter的形状是: (batch_size, num_steps)
+
+    >>> batch_size, num_steps = 32, 128
+    >>> data_iter, vocab = load_data_time_machine(batch_size, num_steps, token='char')
+    >>> for X, y in data_iter:
+    >>>     print(X.shape, y.shape)
+    >>>     break
+    torch.Size([32, 128]) torch.Size([32, 128])
+    """
     data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter,
                               max_tokens, token)
     return data_iter, data_iter.vocab
@@ -687,6 +715,9 @@ def rnn(inputs, state, params):
         H = torch.tanh(torch.mm(X, W_xh) + torch.mm(H, W_hh) + b_h)
         Y = torch.mm(H, W_hq) + b_q
         outputs.append(Y)
+    # 输出:
+    # output的形状:(`时间步数量x批量大小`, `词表大小`)
+    # H的形状:(`批量大小`, `隐藏单元数`)
     return torch.cat(outputs, dim=0), (H, )
 
 
@@ -798,6 +829,8 @@ class RNNModelScratch:
         self.init_state, self.forward_fn = init_state, forward_fn
 
     def __call__(self, X, state):
+        # X的形状是: (`批量大小`，`时间步数量`)
+        # X经过one_hot编码之后的形状是: (`时间步数量`，`批量大小`，`词表大小`)
         X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
         return self.forward_fn(X, state, self.params)
 
@@ -822,8 +855,8 @@ class RNNModule(nn.Module):
     def forward(self, X, state):
         # `X`的形状:(`批量大小`, `时间步数量`)
         # 我们将`X`转置为: (`时间步数量`, `批量大小`)
-        X = F.one_hot(X.T.long(), self.vocab_size)
         # 经过OneHot处理之后为: (`时间步数量`, `批量大小`, `词表大小`), 再传给RNN层
+        X = F.one_hot(X.T.long(), self.vocab_size)
         X = X.to(torch.float32)
         Y, state = self.rnn(X, state)
 
@@ -849,7 +882,17 @@ class RNNModule(nn.Module):
 
 
 def predict_rnn(prefix, num_preds, net, vocab, device):
-    """在`prefix`后面生成新字符"""
+    """
+    在`prefix`后面生成新字符
+    
+    参数:
+    `num_preds`: 根据prefix预热之后, 要预测的步数
+    `vocab`: 由于prefix是文本数据, 需要vocab将prefix转换成整数送入rnn net
+
+    这个例子是一个没有训练过的rnn, 所以输出的是一些随机数据
+    >>> predict_rnn('time traveller ', 10, net, vocab, ml.try_gpu())
+    time traveller vgywzillll
+    """
     state = net.begin_state(batch_size=1, device=device)
     outputs = [vocab[prefix[0]]]
     get_input = lambda: torch.tensor([outputs[-1]], device=device).reshape(
@@ -1047,6 +1090,97 @@ class EncoderDecoder(nn.Module):
         return self.decoder(dec_X, dec_state)
 
 
+class Seq2SeqEncoder(Encoder):
+    """用于序列到序列学习的循环神经网络编码器"""
+    def __init__(self,
+                 vocab_size,
+                 embed_size,
+                 num_hiddens,
+                 num_layers,
+                 dropout=0,
+                 **kwargs):
+        super(Seq2SeqEncoder, self).__init__(**kwargs)
+        # Embedding + GRU
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+
+    def forward(self, X, *args):
+        """
+        分两个部分:
+        1. 使用embedding处理X, 最终得到适合送入RNN的数据
+        2. 使用GRU来处理处理好的X得到输出
+        """
+
+        # `X`输出的形状:(`batch_size`, `num_steps`)
+        # 经过embedding处理之后的形状:(`batch_size`, `num_steps`, `embed_size`)
+        X = self.embedding(X)
+        # 在循环神经网络模型中，第一个轴对应于时间步
+        # 得到最终`X`的形状: (`num_steps`, `batch_size`, `embed_size`)
+        X = X.permute(1, 0, 2)
+
+        # 如果未提及状态, 则默认为0
+        output, state = self.rnn(X)
+        # `output`的形状: (`num_steps`, `batch_size`, `num_hiddens`)
+        # `state`的形状: (`num_layers`, `batch_size`, `num_hiddens`)
+        return output, state
+
+
+class Seq2SeqDecoder(Decoder):
+    def __init__(self,
+                 vocab_size,
+                 embed_size,
+                 num_hiddens,
+                 num_layers,
+                 dropout=0,
+                 **kwargs):
+        """用于序列到序列学习的循环神经网络解码器"""
+        super(Seq2SeqDecoder, self).__init__(**kwargs)
+        # Embedding + GRU + Dense
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        # 注意GRU的输入: 上下文变量在所有的时间步与解码器的输入进行拼接(concatenate),
+        # 也就是: embed_size + num_hiddens
+        self.rnn = nn.GRU(embed_size + num_hiddens,
+                          num_hiddens,
+                          num_layers,
+                          dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, *args):
+        # 直接使用编码器最后一个时间步的隐藏状态来初始化解码器的隐藏状态
+        return enc_outputs[1]
+
+    def forward(self, X, state):
+        """
+        分4个部分:
+        1. 使用embedding处理X, 最终得到适合送入RNN的数据
+        2. 拼接输入X和Encoder的最终state, 得到X_and_context
+        3. 使用GRU来处理X_and_context得到输出output
+        4. 将output送到dense得到最终的结果
+        """
+
+        # `X`输出的形状:(`batch_size`, `num_steps`)
+        # 经过embedding处理之后的形状:(`batch_size`, `num_steps`, `embed_size`)
+        X = self.embedding(X)
+        # 在循环神经网络模型中，第一个轴对应于时间步
+        # 得到最终`X`的形状: (`num_steps`, `batch_size`, `embed_size`)
+        X = X.permute(1, 0, 2)
+
+        # 广播`context`, 使其具有与`X`相同的`num_steps`
+        # 输出`context`的形状: (`num_steps`, `batch_size`, `num_hiddens`)
+        context = state[-1].repeat(X.shape[0], 1,
+                                   1)  # state[-1]的含义是state可能会有多层, 我们只取最后一层
+        # 输出`X_and_context`的形状: (`num_steps`, `batch_size`, `embed_size+num_hiddens`)
+        X_and_context = torch.cat((X, context), 2)
+
+        output, state = self.rnn(X_and_context, state)
+        output = self.dense(output).permute(1, 0, 2)
+
+        # 最终:
+        # `output`的形状: (`batch_size`, `num_steps`, `vocab_size`)
+        # `state`的形状: (`num_layers`, `batch_size`, `num_hiddens`)
+        return output, state
+
+
 def sequence_mask(X, valid_len, value=0):
     """在序列中屏蔽不相关的项"""
     maxlen = X.size(1)
@@ -1155,7 +1289,7 @@ def predict_seq2seq(net,
         # 我们使用具有预测最高可能性的token, 作为解码器在下一时间步的输入
         dec_X = Y.argmax(dim=2)
         pred = dec_X.squeeze(dim=0).type(torch.int32).item()
-        # 保存注意力权重
+        # 保存注意力权重, 每一个step都会保存一个
         if save_attention_weights:
             attention_weight_seq.append(net.decoder.attention_weights)
         # 一旦序列结束词元被预测, 输出序列的生成就完成了
@@ -1759,3 +1893,250 @@ def show_heatmaps(matrices,
             if titles:
                 ax.set_title(titles[j])
     fig.colorbar(pcm, ax=axes, shrink=0.6)
+
+
+def masked_softmax(X, valid_lens):
+    """通过在最后一个轴上(进行softmax计算的轴)遮蔽元素来执行softmax操作"""
+    # `X`: 3D张量, `valid_lens`: 1D或2D张量
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        # 将valid_len转换成1D张量
+        if valid_lens.dim() == 1:
+            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+        else:
+            valid_lens = valid_lens.reshape(-1)
+        # 在最后的轴上, 被遮蔽的元素使用一个非常大的负值替换, 从而其softmax(指数)输出为0
+        X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+        return nn.functional.softmax(X.reshape(shape), dim=-1)
+
+
+class AdditiveAttention(nn.Module):
+    """加性注意力"""
+    def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
+        super(AdditiveAttention, self).__init__(**kwargs)
+        """
+        三个需要学习的参数: W_k, W_q, w_v
+        """
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+        self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    # `queries` 的形状：(`batch_size`, 查询的个数, `query_size`)
+    # `key` 的形状：(`batch_size`, "键－值"对的个数, `key_size`)
+    # `values` 的形状：(`batch_size`, "键－值"对的个数, `value_size`)
+    # `valid_lens` 的形状: (`batch_size`,) 或者 (`batch_size`, 查询的个数)
+    def forward(self, queries, keys, values, valid_lens):
+        # 计算之后:
+        # `queries` 的形状：(`batch_size`, 查询的个数, `num_hidden`)
+        # `key` 的形状：(`batch_size`, "键－值"对的个数, `num_hiddens`)
+        queries, keys = self.W_q(queries), self.W_k(keys)
+        # 在维度扩展后:
+        # `queries` 的形状：(`batch_size`, 查询的个数, 1, `num_hidden`)
+        # `key` 的形状：(`batch_size`, 1, "键－值"对的个数, `num_hiddens`)
+        # 使用广播方式进行求和:
+        # `features` 的形状：(`batch_size`, 查询的个数, "键－值"对的个数, `num_hiddens`)
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        # `self.w_v`仅有一个输出, 因此从形状中移除最后那个维度
+        # `scores` 的形状：(`batch_size`, 查询的个数, "键-值"对的个数)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        # `values` 的形状：(`batch_size`, "键－值"对的个数, `value_size`)
+        # 最终返回的形状: (`batch_size`, 查询的个数, `value_size`)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+
+class DotProductAttention(nn.Module):
+    """缩放点积注意力"""
+    def __init__(self, dropout, **kwargs):
+        super(DotProductAttention, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+
+    # `queries` 的形状：(`batch_size`, 查询的个数, `d`)
+    # `keys` 的形状：(`batch_size`, "键－值"对的个数, `d`)
+    # `values` 的形状：(`batch_size`, "键－值"对的个数, `value_size`)
+    # `valid_lens` 的形状: (`batch_size`,) 或者 (`batch_size`, 查询的个数)
+    def forward(self, queries, keys, values, valid_lens=None):
+        d = queries.shape[-1]
+        # 设置 `transpose_b=True` 为了交换 `keys` 的最后两个维度
+        # `scores` 的形状：(`batch_size`, 查询的个数, "键－值"对的个数)
+        scores = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)
+        # `attention_weights` 的形状：(`batch_size`, 查询的个数, "键－值"对的个数)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        # 最终返回的形状: (`batch_size`, 查询的个数, `value_size`)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+
+class AttentionDecoder(Decoder):
+    """带有注意力机制的解码器基本接口"""
+    def __init__(self, **kwargs):
+        super(AttentionDecoder, self).__init__(**kwargs)
+
+    @property
+    def attention_weights(self):
+        raise NotImplementedError
+
+
+class Seq2SeqAttentionDecoder(AttentionDecoder):
+    def __init__(self,
+                 vocab_size,
+                 embed_size,
+                 num_hiddens,
+                 num_layers,
+                 dropout=0,
+                 **kwargs):
+        """用于序列到序列学习的循环神经网络解码器"""
+        super(Seq2SeqAttentionDecoder, self).__init__(**kwargs)
+        # Embedding + GRU + Dense
+
+        # Attention:
+        # 在这个场景中, `key_size`, `value_size`, `query_size`,
+        # `num_hiddens`都等于num_hiddens
+        #
+        # output = self.attention(queries, keys, values, valid_lens)
+        # queries的形状：(batch_size, 查询的个数, query_size)
+        # keys的形状：(batch_size, "键－值"对的个数, key_size)
+        # values的形状：(batch_size, "键－值"对的个数, value_size)
+        # valid_lens的形状: (batch_size,) 或者 (batch_size, 查询的个数)
+        # output的形状: (batch_size, 查询的个数, value_size)
+        #
+        # `"键－值"对的个数`就是num_steps
+        self.attention = AdditiveAttention(num_hiddens, num_hiddens,
+                                           num_hiddens, dropout)
+
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens,
+                          num_hiddens,
+                          num_layers,
+                          dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens, *args):
+        # 参数的形状:
+        # `enc_outputs`的形状为 (`num_steps`, `batch_size`, `num_hiddens`).
+        # `hidden_state`的形状为 (`num_layers`, `batch_size`, `num_hiddens`)
+        outputs, hidden_state = enc_outputs
+
+        # 返回的形状:
+        # `enc_outputs`的形状为 (`batch_size`, `num_steps`, `num_hiddens`).
+        # `hidden_state`的形状为 (`num_layers`, `batch_size`, `num_hiddens`)
+        return (outputs.permute(1, 0, 2), hidden_state, enc_valid_lens)
+
+    def forward(self, X, state):
+        # `X`的形状为 (`batch_size`, `num_steps`)
+        # `enc_outputs`的形状为 (`batch_size`, `num_steps`, `num_hiddens`).
+        # `hidden_state`的形状为 (`num_layers`, `batch_size`, `num_hiddens`)
+        enc_outputs, hidden_state, enc_valid_lens = state
+        # 输出 `X`的形状为 (`num_steps`, `batch_size`, `embed_size`)
+        X = self.embedding(X).permute(1, 0, 2)
+        outputs, self._attention_weights = [], []
+
+        # 一个step一个step的预测
+        for x in X:
+            # `x`的形状为 (`batch_size`, `embed_size`)
+            # `query`的形状为 (`batch_size`, 1, `num_hiddens`), 其中1为attention query的查询个数
+            query = torch.unsqueeze(hidden_state[-1], dim=1)
+            # `context`的形状为 (`batch_size`, 1, `num_hiddens`)
+            # 这里enc_valid_len的意义在于: 每个小批量当中的序列, 末尾可能是<pad>,
+            # 这部分在计算attention的时候是可以不用看的, 直接忽略掉
+            context = self.attention(query, enc_outputs, enc_outputs,
+                                     enc_valid_lens)
+            # 在特征维度上连结
+            # `x`的形状为 (`batch_size`, 1, `embed_size` + `num_hiddens`)
+            x = torch.cat((context, torch.unsqueeze(x, dim=1)), dim=-1)
+            # 将 `x` 变形为 (1, `batch_size`, `embed_size` + `num_hiddens`)
+            # `out`的形状为 (1, `batch_size`, `num_hiddens`)
+            out, hidden_state = self.rnn(x.permute(1, 0, 2), hidden_state)
+            outputs.append(out)
+            # attention_weights的形状:
+            # (`batch_size`, 查询的个数, "键-值"对的个数) -> (`batch_size`, 1, `num_steps`)
+            self._attention_weights.append(self.attention.attention_weights)
+        # 全连接层变换后， `outputs`的形状为
+        # (`num_steps`, `batch_size`, `vocab_size`)
+        outputs = self.dense(torch.cat(outputs, dim=0))
+
+        # 返回的outputs的形状: (`batch_size`, `num_steps`, `vocab_size`)
+        # `enc_outputs`的形状为 (`batch_size`, `num_steps`, `num_hiddens`).
+        # `hidden_state`的形状为 (`num_layers`, `batch_size`, `num_hiddens`)
+        return outputs.permute(1, 0,
+                               2), [enc_outputs, hidden_state, enc_valid_lens]
+
+    @property
+    def attention_weights(self):
+        return self._attention_weights
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,
+                 key_size,
+                 query_size,
+                 value_size,
+                 num_hiddens,
+                 num_heads,
+                 dropout,
+                 bias=False,
+                 **kwargs):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+        self.num_heads = num_heads
+
+        # 缩放点积注意力作为每一个注意力头
+        self.attention = DotProductAttention(dropout)
+
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
+        self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
+        self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
+
+    def forward(self, queries, keys, values, valid_lens):
+        """
+        当前的版本是效率比较高的实现方式, 我们没有单独计算每个头, 
+        而是通过变换, 并行的计算多个头
+        """
+        # `self.W_q(queries)`, `self.W_q(keys)`, or `self.W_q(values)` 的形状:
+        # (`batch_size`, 查询或者"键－值"对的个数, `num_hiddens`)
+        # `valid_lens`　的形状:
+        # (`batch_size`,) or (`batch_size`, 查询的个数)
+        #
+        # 经过变换后, 输出的 `queries`, `keys`, or `values`　的形状:
+        # (`batch_size` * `num_heads`, 查询或者“键－值”对的个数, `num_hiddens` / `num_heads`)
+        queries = transpose_qkv(self.W_q(queries), self.num_heads)
+        keys = transpose_qkv(self.W_k(keys), self.num_heads)
+        values = transpose_qkv(self.W_v(values), self.num_heads)
+
+        if valid_lens is not None:
+            # 在轴0, 将第一项（标量或者矢量）复制 `num_heads` 次,
+            # 然后如此复制第二项，然后诸如此类。
+            valid_lens = torch.repeat_interleave(valid_lens,
+                                                 repeats=self.num_heads,
+                                                 dim=0)
+
+        # `output` 的形状: (`batch_size` * `num_heads`, 查询的个数, `num_hiddens` / `num_heads`)
+        output = self.attention(queries, keys, values, valid_lens)
+
+        # `output_concat` 的形状: (`batch_size`, 查询的个数, `num_hiddens`)
+        output_concat = transpose_output(output, self.num_heads)
+        return self.W_o(output_concat)
+
+
+def transpose_qkv(X, num_heads):
+    # 输入 `X` 的形状: (`batch_size`, 查询或者"键－值"对的个数, `num_hiddens`).
+    # 输出 `X` 的形状: (`batch_size`, 查询或者"键－值"对的个数, `num_heads`, `num_hiddens` / `num_heads`)
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+
+    # 输出 `X` 的形状: (`batch_size`, `num_heads`, 查询或者“键－值”对的个数, `num_hiddens` / `num_heads`)
+    X = X.permute(0, 2, 1, 3)
+
+    # `output` 的形状: (`batch_size` * `num_heads`, 查询或者“键－值”对的个数, `num_hiddens` / `num_heads`)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+
+def transpose_output(X, num_heads):
+    """逆转`transpose_qkv`函数的操作"""
+    # 输入X的形状: (`batch_size` * `num_heads`, 查询的个数, `num_hiddens` / `num_heads`)
+    X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
+    X = X.permute(0, 2, 1, 3)
+    # 的形状: (`batch_size`, 查询的个数, `num_hiddens`)
+    return X.reshape(X.shape[0], X.shape[1], -1)
